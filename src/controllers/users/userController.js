bfb1b52d419cf1316d8abdac6219c39caa498664
Asier <asierherrera97@gmail.com>
2024-05-22 13:42:16 +0200

    Login y register funcionan

--- src/controllers/users/userController.js
+++ src/controllers/users/userController.js
@@ -1,4 +1,6 @@
 import userModel from "../../models/userModel.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const getAll = async()=> {
     try {
@@ -27,8 +29,63 @@ const getByProperty = async(property,value) =>{
         return null;
     }
 }
+
+const login = async(data) =>{
+    const {email,username,password} = data;
+    if((!email && !username ) || !password){
+        return {error:"faltan datos",status:400};
+    }
+    try {
+        let user;
+        if(email){
+            const users = await getByProperty("email",email);
+            user = users[0];
+        }
+        else{
+            const users = await getByProperty("username",username);
+            user = users[0];
+        }
+        console.log("usurio",user);
+        if(!user){
+            return {error:"No existe el usurio",status:400};
+        }
+        console.log("contraseña",password,user.password);
+        const isPasswordCorrect = await bcrypt.compare(password,user.password);
+        if(!isPasswordCorrect){
+            return {error:"Combinación de usuario y contraseña erroneos",status:400};
+        }
+        console.log("login user",user)
+        const token = jwt.sign({_id:user._id,username:user.username,role:user.role},process.env.JWT_SECRET,{expiresIn: 60 * 60})
+        return {token};
+
+        
+    } catch (error) {
+        console.error(error);
+        return {error:"Ha habido un error",status:500};
+    }
+}
+const register = async(data) => {
+    const {email,username,password,passwordRepeat} = data;
+    if(!email || !username || !password || !passwordRepeat){
+        return {error:"Falta alguno de los campos"};
+    }
+    if(password !== passwordRepeat){
+        return {error:"Las contraseñas no coinciden"};
+    }
+    const userData = {
+        email,
+        username,
+        password,
+        role:"user"
+    }
+    const user = await create(userData);
+    return user;
+}
+
 const create = async(data) =>{
     try {
+        const hash = await bcrypt.hash(data.password,10);
+        data.password = hash;
         const user = await userModel.create(data);
         return user;
     } catch (error) {
@@ -61,6 +118,8 @@ export const functions = {
     getAll,
     getById,
     getByProperty,
+    login,
+    register,
     create,
     update,
     remove
