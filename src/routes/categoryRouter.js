da01db6b5b2073400f0d82dd70dbf4e4f2934029
Asier <asierherrera97@gmail.com>
2024-05-24 10:00:49 +0200

    Version presentacion

--- src/routes/categoryRouter.js
+++ src/routes/categoryRouter.js
@@ -1,6 +1,7 @@
 import {Router} from "express";
 
 import categoryApiController from "../controllers/categories/categoryApiController.js";
+import { isAuthenticated,isAdmin } from "../middlewares/authMiddleware.js";
 
 
 const router  = Router();
@@ -10,8 +11,8 @@ router.post("/",categoryApiController.create);
 router.get('/search', categoryApiController.getByProperty);
 router.get('/', categoryApiController.getAllCategories);
 router.get("/categories/:category",categoryApiController.getByCategory);
-router.put("/:id",categoryApiController.update);
-router.delete("/:id",categoryApiController.remove);
+router.put("/:id",isAdmin,categoryApiController.update);
+router.delete("/:id",isAdmin,categoryApiController.remove);
 router.get("/:id",categoryApiController.getById);
 /* router.post("/:id/status",categoryApiController.changeStatus);
 router.post("/:id/user",categoryApiController.addUser);
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,15 +1,15 @@
 import {Router} from "express";
 
 import userApiController from "../controllers/users/userApiController.js";
-
+import { isAuthenticated,isAdmin } from "../middlewares/authMiddleware.js";
 
 const router  = Router();
 
-router.get("/",userApiController.getAll);
-router.get("/byproperty",userApiController.getByProperty);
-router.get("/:id",userApiController.getById);
+router.get("/",isAdmin,userApiController.getAll);
+router.get("/byproperty",isAdmin,userApiController.getByProperty);
+router.get("/:id",isAdmin,userApiController.getById);
 router.post("/",userApiController.create);
 router.put("/:id",userApiController.update);
-router.delete("/:id",userApiController.remove);
+router.delete("/:id",isAdmin,userApiController.remove);
 
 export default router;
